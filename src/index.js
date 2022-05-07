79ac20333af5b40ed27cea8f710e845c33427c58
Vikas Ekka <vikasekka08@gmail.com>
2022-05-07 18:53:11 +0530

    login added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-m6svv16c.us.auth0.com"
+    clientId='ryIhzJXR7USFpvdARtEzvK5E3KYn5zx7'
+    redirectUri={window.location.origin}>      
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
