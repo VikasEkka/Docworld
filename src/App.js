097679c871c3b2991b3c655433692e5a9b4a7c82
Vikas Ekka <vikasekka08@gmail.com>
2022-05-07 18:57:20 +0530

    logout added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
-  const {loginWithRedirect} = useAuth0();
+  const {loginWithRedirect,user,logout,isAuthenticated} = useAuth0();
   return (
     <div >
       <h1>Welcome to Docworld</h1>
       <button onClick={loginWithRedirect}>Login</button>
+      <button onClick={logout}>Logout</button>
+      <h3>User is {isAuthenticated ? "logged in " : "not logged in "}</h3>
     </div>
   );
 }
